da7fdc931ddd3f09d445960abfe187ace7f54468
Andrew Sieg <ASieg@humana.com>
2020-07-17 16:19:40 -0400

    adding new url

--- application.py
+++ application.py
@@ -7,12 +7,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    url = "http://127.0.0.1:5000/"
+    url = "https://app-communicaton-test-2.azurewebsites.net/"
     res = requests.get(url)
     dictFromServer = res.json()
     return dictFromServer['message']
 
 
 if __name__=='__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0')
    
